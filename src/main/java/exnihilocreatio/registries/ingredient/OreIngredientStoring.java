6911c8a9872e14bee4bcf6bc0b37a4d23866c36d
BloodWorkXGaming <BloodWorkXGaming@users.noreply.github.com>
2018-03-13 01:16:54 +0100

    removed recently added dynamic stuff as it still points to the oredict even if it changes

--- src/main/java/exnihilocreatio/registries/ingredient/OreIngredientStoring.java
+++ src/main/java/exnihilocreatio/registries/ingredient/OreIngredientStoring.java
@@ -1,12 +1,6 @@
 package exnihilocreatio.registries.ingredient;
 
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.NonNullList;
-import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.OreIngredient;
-
-import javax.annotation.Nonnull;
 
 /**
  * Exactly the same as OreIngredient, but it still has the name in it so we can store it to JSON, YAML or whatever.
@@ -21,39 +15,5 @@ public class OreIngredientStoring extends OreIngredient {
 
     public String getOreName() {
         return oreName;
-    }
-
-    @Override
-    public boolean apply(ItemStack input) {
-        if (input == null || input.isEmpty())
-            return false;
-        if (!OreDictionary.doesOreNameExist(oreName))
-            return false;
-
-        for (ItemStack stack : OreDictionary.getOres(oreName, false)) {
-            if (OreDictionary.itemMatches(stack, input, false))
-                return true;
-        }
-
-        return false;
-    }
-
-    @Nonnull
-    @Override
-    public ItemStack[] getMatchingStacks() {
-        NonNullList<ItemStack> items = OreDictionary.getOres(oreName);
-        NonNullList<ItemStack> list = NonNullList.create();
-
-        if (items != null && !items.isEmpty()) {
-            for (ItemStack itemstack : items)
-            {
-                if (itemstack.getMetadata() == OreDictionary.WILDCARD_VALUE)
-                    itemstack.getItem().getSubItems(CreativeTabs.SEARCH, list);
-                else
-                    list.add(itemstack);
-            }
-        }
-
-        return list.toArray(new ItemStack[0]);
     }
 }
