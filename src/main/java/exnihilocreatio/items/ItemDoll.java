f4175ec04cbaab08874df5e6166814b9aa46ab71
BloodWorkXGaming <bloodworkxgaming@gmail.com>
2018-06-21 22:38:55 +0200

    Attempt at fixing dolls

--- src/main/java/exnihilocreatio/items/ItemDoll.java
+++ src/main/java/exnihilocreatio/items/ItemDoll.java
@@ -4,6 +4,8 @@ import exnihilocreatio.ExNihiloCreatio;
 import exnihilocreatio.ModFluids;
 import exnihilocreatio.util.Data;
 import exnihilocreatio.util.IHasModel;
+import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.minecraft.client.renderer.block.model.ModelBakery;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.util.ITooltipFlag;
@@ -27,37 +29,11 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ItemDoll extends Item implements IHasModel {
 
-    public enum DollType {
-        BLAZE(0, "blaze", "minecraft:blaze", "lava", 1),
-        ENDERMAN(1, "enderman", "minecraft:enderman", "witchwater", 2),
-        SHULKER(2, "shulker", "minecraft:shulker", "witchwater", 1.5),
-        GUARDIAN(3, "guardian", "minecraft:guardian", "water", 1),
-        BLIZZ(4, "blizz", "thermalfoundation:blizz", "pyrotheum", 1),
-        BLITZ(5, "blitz", "thermalfoundation:blitz", "pyrotheum", 1),
-        BASALZ(6, "basalz", "thermalfoundation:basalz", "pyrotheum", 1),
-        BLUESLIME(7, "blueslime", "tconstruct:blueslime", "milk", 2);
-
-        public final int meta;
-        public final String name;
-        public final String entityname;
-        public final String fluidname;
-        public final double posYCorrection;
-
-        private DollType(int meta, String name, String entityname, String fluidname, double posYCorrection) {
-            this.meta = meta;
-            this.name = name;
-            this.entityname = entityname;
-            this.fluidname = fluidname;
-            this.posYCorrection = posYCorrection;
-        }
-    }
-
-    private static final ArrayList<DollType> types = new ArrayList<>();
+    private static final Int2ObjectMap<DollType> existingTypes = new Int2ObjectArrayMap<>();
 
     public ItemDoll() {
         super();
@@ -67,26 +43,26 @@ public class ItemDoll extends Item implements IHasModel {
         setCreativeTab(ExNihiloCreatio.tabExNihilo);
         setHasSubtypes(true);
 
-        types.add(DollType.BLAZE.meta,DollType.BLAZE);
-        types.add(DollType.ENDERMAN.meta,DollType.ENDERMAN);
-        types.add(DollType.SHULKER.meta,DollType.SHULKER);
-        types.add(DollType.GUARDIAN.meta,DollType.GUARDIAN);
+        existingTypes.put(DollType.BLAZE.meta, DollType.BLAZE);
+        existingTypes.put(DollType.ENDERMAN.meta, DollType.ENDERMAN);
+        existingTypes.put(DollType.SHULKER.meta, DollType.SHULKER);
+        existingTypes.put(DollType.GUARDIAN.meta, DollType.GUARDIAN);
 
         if (Loader.isModLoaded("thermalfoundation")) {
-            types.add(DollType.BLIZZ.meta,DollType.BLIZZ);
-            types.add(DollType.BLITZ.meta,DollType.BLITZ);
-            types.add(DollType.BASALZ.meta,DollType.BASALZ);
+            existingTypes.put(DollType.BLIZZ.meta, DollType.BLIZZ);
+            existingTypes.put(DollType.BLITZ.meta, DollType.BLITZ);
+            existingTypes.put(DollType.BASALZ.meta, DollType.BASALZ);
         }
 
         if (Loader.isModLoaded("tconstruct")) {
-            types.add(DollType.BLUESLIME.meta,DollType.BLUESLIME);
+            existingTypes.put(DollType.BLUESLIME.meta, DollType.BLUESLIME);
         }
 
         Data.ITEMS.add(this);
     }
 
     public Fluid getSpawnFluid(ItemStack stack) {
-        Fluid fluid = FluidRegistry.getFluid(types.get(stack.getMetadata()).fluidname);
+        Fluid fluid = FluidRegistry.getFluid(existingTypes.get(stack.getMetadata()).fluidname);
         if (fluid != null)
             return fluid;
         else
@@ -101,7 +77,7 @@ public class ItemDoll extends Item implements IHasModel {
      * @return true if spawn is successful
      */
     public boolean spawnMob(ItemStack stack, World world, BlockPos pos) {
-        DollType type = types.get(stack.getMetadata());
+        DollType type = existingTypes.get(stack.getMetadata());
         if (type == null) return false;
 
         Entity spawnee = EntityList.createEntityByIDFromName(new ResourceLocation(type.entityname), world);
@@ -115,14 +91,14 @@ public class ItemDoll extends Item implements IHasModel {
     @Override
     @Nonnull
     public String getUnlocalizedName(ItemStack stack) {
-        return getUnlocalizedName() + "." + types.get(stack.getItemDamage()).name;
+        return getUnlocalizedName() + "." + DollType.getByMeta(stack.getItemDamage()).name;
     }
 
     @Override
     @SideOnly(Side.CLIENT)
     public void getSubItems(@Nullable CreativeTabs tab, @Nonnull NonNullList<ItemStack> list) {
         if (this.isInCreativeTab(tab))
-            for (DollType type : types) {
+            for (DollType type : existingTypes.values()) {
                 list.add(new ItemStack(this, 1, type.meta));
             }
     }
@@ -131,21 +107,58 @@ public class ItemDoll extends Item implements IHasModel {
     @SideOnly(Side.CLIENT)
     public void initModel(ModelRegistryEvent event) {
 
-        List<ModelResourceLocation> locations = new ArrayList<>();
-        for (DollType type : types) {
-            locations.add(new ModelResourceLocation(getRegistryName(), "type=" + type.name));
+        Int2ObjectMap<ModelResourceLocation> locations = new Int2ObjectArrayMap<>();
+        for (DollType type : existingTypes.values()) {
+            locations.put(type.meta, new ModelResourceLocation(getRegistryName(), "type=" + type.name));
         }
 
-        ModelBakery.registerItemVariants(this, locations.toArray(new ModelResourceLocation[0]));
+        ModelBakery.registerItemVariants(this, locations.values().toArray(new ModelResourceLocation[0]));
         ModelLoader.setCustomMeshDefinition(this, stack -> locations.get(stack.getMetadata()));
 
     }
 
     @Override
     @SideOnly(Side.CLIENT)
-    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
-    {
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
         tooltip.add(I18n.translateToLocal(getUnlocalizedName(stack) + ".desc"));
+    }
+
+    public enum DollType {
+        // @formatter:off
+        BLAZE(      0, "blaze",     "minecraft:blaze",          "lava",         1),
+        ENDERMAN(   1, "enderman",  "minecraft:enderman",       "witchwater",   2),
+        SHULKER(    2, "shulker",   "minecraft:shulker",        "witchwater",   1.5),
+        GUARDIAN(   3, "guardian",  "minecraft:guardian",       "water",        1),
+        BLIZZ(      4, "blizz",     "thermalfoundation:blizz",  "pyrotheum",    1),
+        BLITZ(      5, "blitz",     "thermalfoundation:blitz",  "pyrotheum",    1),
+        BASALZ(     6, "basalz",    "thermalfoundation:basalz", "pyrotheum",    1),
+        BLUESLIME(  7, "blueslime", "tconstruct:blueslime",      "milk",        2);
+        // @formatter:on
+        private static final Int2ObjectMap<DollType> ALL_TYPES = new Int2ObjectArrayMap<>();
+
+        static {
+            for (DollType dollType : values()) {
+                ALL_TYPES.put(dollType.meta, dollType);
+            }
+        }
+
+        public final int meta;
+        public final String name;
+        public final String entityname;
+        public final String fluidname;
+        public final double posYCorrection;
+
+        DollType(int meta, String name, String entityname, String fluidname, double posYCorrection) {
+            this.meta = meta;
+            this.name = name;
+            this.entityname = entityname;
+            this.fluidname = fluidname;
+            this.posYCorrection = posYCorrection;
+        }
+
+        public static DollType getByMeta(int meta) {
+            return ALL_TYPES.get(meta);
+        }
     }
 
 }
