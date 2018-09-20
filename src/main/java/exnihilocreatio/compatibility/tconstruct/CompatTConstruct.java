c51dd54040625cb77517268fb346574fbd8aa612
SirLyle <infraredhat@gmail.com>
2018-09-19 20:24:53 -0400

    Tinker's Crook and some modifier textures for the hammer.

--- src/main/java/exnihilocreatio/compatibility/tconstruct/CompatTConstruct.java
+++ src/main/java/exnihilocreatio/compatibility/tconstruct/CompatTConstruct.java
@@ -1,5 +1,6 @@
 package exnihilocreatio.compatibility.tconstruct;
 
+import exnihilocreatio.ExNihiloCreatio;
 import exnihilocreatio.ModItems;
 import exnihilocreatio.config.ModConfig;
 import exnihilocreatio.items.ore.EnumOreSubtype;
@@ -7,6 +8,7 @@ import exnihilocreatio.items.ore.ItemOre;
 import exnihilocreatio.registries.manager.ExNihiloRegistryManager;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -16,25 +18,54 @@ import slimeknights.tconstruct.library.TinkerRegistry;
 import slimeknights.tconstruct.library.TinkerRegistryClient;
 import slimeknights.tconstruct.library.client.ToolBuildGuiInfo;
 import slimeknights.tconstruct.library.materials.Material;
+import slimeknights.tconstruct.library.modifiers.IModifier;
 import slimeknights.tconstruct.library.modifiers.Modifier;
+import slimeknights.tconstruct.tools.TinkerModifiers;
 
 public class CompatTConstruct {
     public static SledgeHammer sledgeHammer = new SledgeHammer();
+    public static TiCrook crook = new TiCrook();
 
     public static void registerItems(IForgeRegistry<Item> registry) {
         // Register Sledge Hammer
         registry.register(sledgeHammer);
         TinkerRegistry.registerToolCrafting(sledgeHammer);
         ModelRegisterUtil.registerToolModel(sledgeHammer);
+        // Register Crook
+        registry.register(crook);
+        TinkerRegistry.registerToolCrafting(crook);
+        ModelRegisterUtil.registerToolModel(crook);
+
+        // Register Modifiers
+        for (IModifier modifier: new IModifier[] {
+                TinkerModifiers.modDiamond,
+                TinkerModifiers.modEmerald,
+                TinkerModifiers.modGlowing,
+                TinkerModifiers.modHaste,
+                TinkerModifiers.modKnockback,
+                TinkerModifiers.modLuck,
+                TinkerModifiers.modMendingMoss,
+                TinkerModifiers.modReinforced,
+                TinkerModifiers.modSoulbound,
+        }) {
+            ModelRegisterUtil.registerModifierModel(modifier, new ResourceLocation(ExNihiloCreatio.MODID, "models/item/tools/modifiers/"+modifier.getIdentifier()));
+        }
     }
 
     public static void initClient(FMLInitializationEvent event) {
+        ToolBuildGuiInfo info;
         // Sledge Hammer Crafting
-        ToolBuildGuiInfo sledgeHammerInfo = new ToolBuildGuiInfo(sledgeHammer);
-        sledgeHammerInfo.addSlotPosition(33 - 18, 42 + 18); // Handle
-        sledgeHammerInfo.addSlotPosition(33 + 20, 42 - 20); // Head
-        sledgeHammerInfo.addSlotPosition(33, 42); // Binding
-        TinkerRegistryClient.addToolBuilding(sledgeHammerInfo);
+        info = new ToolBuildGuiInfo(sledgeHammer);
+        info.addSlotPosition(33 - 18, 42 + 18); // Handle
+        info.addSlotPosition(33 + 20, 42 - 20); // Head
+        info.addSlotPosition(33, 42); // Binding
+        TinkerRegistryClient.addToolBuilding(info);
+        // Crook Crafting
+        info = new ToolBuildGuiInfo(crook);
+        info.addSlotPosition(33 - 11, 42 + 11); // handle
+        info.addSlotPosition(33 - 2, 42 - 20); // head
+        info.addSlotPosition(33 + 18, 42 - 8); // binding
+        TinkerRegistryClient.addToolBuilding(info);
     }
 
     public static void postInit() {
