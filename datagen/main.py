2c8ac35b0b55179b036c682a9f93de977933dcae
DeatHunter <desuhansa@gmail.com>
2022-01-09 19:09:53 +0100

    Add ore piece recipes

--- datagen/main.py
+++ datagen/main.py
@@ -1,12 +1,14 @@
 import barrel_gen
 import crucible_gen
 import sieve_gen
+import ore_gen
 
 
 def main():
     barrel_gen.generate()
     crucible_gen.generate()
     sieve_gen.generate()
+    ore_gen.generate()
 
 
 if __name__ == "__main__":
