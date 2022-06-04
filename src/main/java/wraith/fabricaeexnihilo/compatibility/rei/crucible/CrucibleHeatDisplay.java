eb48674c65c206f000e32dcbd321921a3a5012ff
LordDeatHunter <desuhansa@gmail.com>
2022-06-04 17:56:18 +0200

    Fix fluid display in some recipes

--- src/main/java/wraith/fabricaeexnihilo/compatibility/rei/crucible/CrucibleHeatDisplay.java
+++ src/main/java/wraith/fabricaeexnihilo/compatibility/rei/crucible/CrucibleHeatDisplay.java
@@ -4,6 +4,7 @@ import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.display.Display;
 import me.shedaniel.rei.api.common.entry.EntryIngredient;
 import me.shedaniel.rei.api.common.util.EntryIngredients;
+import net.minecraft.fluid.Fluids;
 import wraith.fabricaeexnihilo.compatibility.rei.PluginEntry;
 import wraith.fabricaeexnihilo.recipe.crucible.CrucibleHeatRecipe;
 
@@ -16,7 +17,13 @@ public class CrucibleHeatDisplay implements Display {
     private final List<EntryIngredient> inputs;
 
     public CrucibleHeatDisplay(CrucibleHeatRecipe recipe) {
-        this.inputs = recipe.getBlock().flatten(EntryIngredients::of);
+        this.inputs = recipe.getBlock().flatten(block -> {
+            var fluid = block.getDefaultState().getFluidState().getFluid();
+            if (fluid != Fluids.EMPTY) {
+                return EntryIngredients.of(fluid);
+            }
+            return EntryIngredients.of(block);
+        });
         this.heat = recipe.getHeat();
     }
 
