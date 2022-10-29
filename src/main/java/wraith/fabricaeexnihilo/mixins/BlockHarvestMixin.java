1b543f6e70cfcc7918c3d233ca973db83de26b84
MattiDragon <61198884+MattiDragon@users.noreply.github.com>
2022-10-29 15:58:44 +0300

    Fix crook voiding blocks

--- src/main/java/wraith/fabricaeexnihilo/mixins/BlockHarvestMixin.java
+++ src/main/java/wraith/fabricaeexnihilo/mixins/BlockHarvestMixin.java
@@ -17,15 +17,17 @@ import java.util.List;
 
 @Mixin(AbstractBlock.class)
 public abstract class BlockHarvestMixin {
-    
     /**
      * Injects calls to the Hammer and Crook registries if the tool used is identified as a Hammer or Crook
      */
-    @Inject(at = @At("RETURN"), method = "getDroppedStacks(Lnet/minecraft/block/BlockState;Lnet/minecraft/loot/context/LootContext$Builder;)Ljava/util/List;", cancellable = true)
+    @Inject(at = @At("RETURN"), method = "getDroppedStacks", cancellable = true)
     public void getDroppedStacks(BlockState state, LootContext.Builder builder, CallbackInfoReturnable<List<ItemStack>> info) {
         ItemStack tool = builder.get(LootContextParameters.TOOL);
         if (CrookItem.isCrook(tool) || HammerItem.isHammer(tool)) {
             var recipes = ToolRecipe.find(CrookItem.isCrook(tool) ? ToolRecipe.ToolType.CROOK : ToolRecipe.ToolType.HAMMER, state.getBlock(), builder.getWorld());
+            // Non-tool block. Just use default drops
+            if (recipes.isEmpty())
+                return;
             info.setReturnValue(recipes.stream()
                     .map(ToolRecipe::getResult)
                     .map(loot -> loot.createStack(builder.getWorld().random))
