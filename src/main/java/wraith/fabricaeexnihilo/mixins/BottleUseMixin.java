b33a8eb2641ccc1937e2d5944f890248f9cc427a
MattiDragon <61198884+MattiDragon@users.noreply.github.com>
2023-07-19 16:09:24 +0300

    Update mixin extras and improve bottle hook

--- src/main/java/wraith/fabricaeexnihilo/mixins/BottleUseMixin.java
+++ src/main/java/wraith/fabricaeexnihilo/mixins/BottleUseMixin.java
@@ -1,11 +1,15 @@
 package wraith.fabricaeexnihilo.mixins;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.GlassBottleItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -19,9 +23,9 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import wraith.fabricaeexnihilo.modules.ModItems;
 import wraith.fabricaeexnihilo.util.ItemUtils;
 
-//TODO: Replace by UseBlockCallback
 @Mixin(GlassBottleItem.class)
 public abstract class BottleUseMixin extends Item {
     public BottleUseMixin(Settings settings) {
@@ -31,23 +35,10 @@ public abstract class BottleUseMixin extends Item {
     @Shadow
     abstract protected ItemStack fill(ItemStack emptyBottle, PlayerEntity player, ItemStack filledBottle);
 
-    @Inject(at = @At("RETURN"), method = "use", cancellable = true)
-    public void use(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir) {
-        ItemStack held = cir.getReturnValue().getValue();
-        if (held.getItem() instanceof GlassBottleItem) {
-            BlockHitResult hitResult = raycast(world, user, RaycastContext.FluidHandling.SOURCE_ONLY);
-            if (hitResult.getType() == HitResult.Type.BLOCK) {
-                BlockPos blockPos = hitResult.getBlockPos();
-                if (world.canPlayerModifyAt(user, blockPos)) {
-                    BlockState target = world.getBlockState(blockPos);
-                    ItemStack result = target.getBlock() != Blocks.SAND ? ItemStack.EMPTY : new ItemStack(ItemUtils.getExNihiloItem("salt_bottle"));
-                    if (!result.isEmpty()) {
-                        cir.setReturnValue(new TypedActionResult<>(ActionResult.SUCCESS, this.fill(held, user, result)));
-                        return;
-                    }
-                }
-            }
-        }
-        cir.cancel();
+    @ModifyReturnValue(method = "use", at = @At("TAIL"))
+    private TypedActionResult<ItemStack> fabricaeexnihilo$collectSalt(TypedActionResult<ItemStack> original, World world, PlayerEntity user, Hand hand, @Local BlockHitResult hitResult) {
+        if (hitResult.getType() != HitResult.Type.BLOCK) return original;
+        if (world.getBlockState(hitResult.getBlockPos()).isIn(BlockTags.SAND)) return original;
+        return TypedActionResult.success(this.fill(original.getValue(), user, new ItemStack(ModItems.SALT_BOTTLE)));
     }
 }
